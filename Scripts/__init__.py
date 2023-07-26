06caf0b9dcf3e48ec9f844ab9d277987975f28a2
Prash099 <prashanthtech099@gmail.com>
2023-07-26 05:42:40 +0200

    Initial Commit without model

---
+++ Scripts/__init__.py
@@ -0,0 +1,8 @@
+MODEL_QA_NAME = "models/"
+SEGMENT_SIZE = 10
+MODEL_PASSAGES_NAME="cs608/billsum-full-data"
+
+API_URL = "https://api-inference.huggingface.co/models/zuu/automatic-speech-recognition"
+API_URL_PASSAGE = "https://api-inference.huggingface.co/models/cs608/billsum-full-data"
+API_TOKEN = "YOUR_API_TOKEN"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
---
+++ frontend.py
@@ -0,0 +1,155 @@
+import requests
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Audio Summarization', layout='wide')
+
+def extract_text_from_website(url):
+    response = requests.post("http://127.0.0.1:5000/extract_url_text", json={"url": url})
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("extracted_text")
+    else:
+        return None
+    
+def extract_text_from_youtube(youtube_link):
+    response = requests.post("http://127.0.0.1:5000/extract_audio_youtube", json={"youtube_link": youtube_link})
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("audio_transcript")
+    else:
+        return None
+
+def extract_text_from_local_audio(audio_file):
+    files = {'audio': audio_file}
+    response = requests.post("http://127.0.0.1:5000/extract_text", files=files)
+    if response.status_code == 200:
+        extracted_text = response.json().get('text')
+        return extracted_text
+    else:
+        return "Text extraction failed. Please try again later."
+
+def generate_summary_passages(text):
+    response = requests.post("http://127.0.0.1:5000/summarize_passages", json={"text": text})
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("summary")
+    else:
+        return None
+
+def generate_qa_summary(text):
+    response = requests.post("http://127.0.0.1:5000/summarize_qa", json={"text": text})
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("summary")
+    else:
+        return None
+
+def make_summarization():
+    st.title('Audio Summarization')
+    st.write('Enter your audio or video URL, upload an audio file, or provide a text transcript.')
+
+    input_option = st.selectbox('Select your input:', ['YouTube URL', 'Local Audio', 'Web URL', 'Text Transcript'])
+
+    if input_option == 'YouTube URL':
+        youtube_link = st.text_input('Enter URL:', value='Your URL')
+        extract_text_option = st.button('Text Extraction')
+        extracted_text = st.empty()
+        if extract_text_option:
+            extracted_text.write('Extracted text from the YouTube URL:')
+            extracted_text_input = st.text_input('', extract_text_from_youtube(youtube_link))
+            st.session_state['youtube_text'] = extracted_text_input
+
+        if st.button('Edit'):
+            extracted_text.write('Extracted text from the YouTube URL:')
+            extracted_text_input = st.text_input('', st.session_state['youtube_text'])
+            st.session_state['youtube_text'] = extracted_text_input
+
+        if st.button('Summarize'):
+            st.write('Extracted text from the YouTube URL:')
+            st.write(st.session_state['youtube_text'])
+            st.write('Summarized result from the YouTube URL:')
+            summarized_result = generate_summary_passages(st.session_state['youtube_text'])
+            if summarized_result:
+                st.write(summarized_result)
+
+        st.experimental_set_query_params(youtube_link=youtube_link)
+
+    elif input_option == 'Web URL':
+        url = st.text_input('Enter URL:', value='Your URL')
+        extract_text_option = st.button('Text Extraction')
+        extracted_text = st.empty()
+        if extract_text_option:
+            extracted_text.write('Extracted text from the website URL:')
+            extracted_text_input = st.text_input('', extract_text_from_website(url))
+            st.session_state['web_text'] = extracted_text_input
+
+        if st.button('Edit'):
+            extracted_text.write('Extracted text from the website URL:')
+            extracted_text_input = st.text_input('', st.session_state['web_text'])
+            st.session_state['web_text'] = extracted_text_input
+
+        if st.button('Summarize'):
+            st.write('Extracted text from the website URL:')
+            st.write(st.session_state['web_text'])
+            st.write('Summarized result from the website URL:')
+            summarized_result = generate_summary_passages(st.session_state['web_text'])
+            if summarized_result:
+                st.write(summarized_result)
+
+        st.experimental_set_query_params(url=url)
+
+    elif input_option == 'Local Audio':
+        local_audio = st.file_uploader('Upload local audio file:', type=['mp3', 'wav'])
+        extract_text_option = st.button('Text Extraction')
+        extracted_text = st.empty()
+        if extract_text_option:
+            extracted_text.write('Extracted text from local audio file:')
+            extracted_text_input = st.text_input('', extract_text_from_local_audio(local_audio))
+            st.session_state['audio_text'] = extracted_text_input
+
+        if st.button('Edit'):
+            extracted_text.write('Extracted text from local audio file:')
+            extracted_text_input = st.text_input('', st.session_state['audio_text'])
+            st.session_state['audio_text'] = extracted_text_input
+
+        if st.button('Summarize'):
+            st.write('Extracted text from local audio file:')
+            st.write(st.session_state['audio_text'])
+            st.write('Summarized result from local audio file:')
+            summarized_result = generate_qa_summary(st.session_state['audio_text'])
+            if summarized_result:
+                st.write(summarized_result)
+
+        st.experimental_set_query_params(local_audio=local_audio)
+
+    elif input_option == 'Text Transcript':
+        text_transcript = st.text_area('Enter text transcript:', value='Your text transcript')
+
+        if st.button('Summarize'):
+            st.write('Extracted text from text transcript:')
+            st.write(text_transcript)
+            st.write('Summarized result from text transcript:')
+            summarized_result = generate_summary_passages(text_transcript)
+            if summarized_result:
+                st.write(summarized_result)
+
+        st.experimental_set_query_params(text_transcript=text_transcript)
+
+def show_past_summaries():
+    st.title('Past Summaries')
+
+    past_summaries = pd.DataFrame({
+        'ID': [1, 2, 3, 4],
+        'Type': ['YouTube URL', 'Local Audio', 'Web URL', 'Text Transcript'],
+        'Transcript': ['', '', '', ''],
+        'Summary': ['', '', '', '']
+    })
+    st.table(past_summaries)
+
+nav_option = st.sidebar.selectbox('Select page:', ['Make Summarization', 'Past Summaries'])
+
+if nav_option == 'Make Summarization':
+    make_summarization()
+elif nav_option == 'Past Summaries':
+    show_past_summaries()
