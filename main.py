c526ace031f2ce2a651501b56435ac98eebf9e44
Prash099 <prashanthtech099@gmail.com>
2023-07-27 21:57:02 +0200

    Deploy API

--- main.py
+++ main.py
@@ -129,4 +129,4 @@ def extract_text_from_audio():
             return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
